274125e5b57019cecb0d8821d2442102821f2cd0
Zoomzoomcarboi <COOLFABIO2@GMAIL.COM>
2025-08-21 05:32:17 -0700

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -82,8 +82,8 @@ class CarInterface(CarInterfaceBase):
     if candidate == CAR.KIA_OPTIMA_G4_FL:
       ret.steerActuatorDelay = 0.2
 
-    if candidate in (CAR.AZERA_6TH_GEN, CAR.AZERA_HEV_6TH_GEN):
-      ret.mass = 1600. if candidate == CAR.AZERA_6TH_GEN else 1675.  # ICE is ~average of 2.5L and 3.5L
+    if candidate in (CAR.HYUNDAI_AZERA_6TH_GEN, CAR.HYUNDAI_AZERA_HEV_6TH_GEN):
+      ret.mass = 1600. if candidate == CAR.HYUNDAI_AZERA_6TH_GEN else 1675.  # ICE is ~average of 2.5L and 3.5L
       ret.wheelbase = 2.885
       ret.steerRatio = 14.5
     elif candidate in (CAR.SANTA_FE, CAR.SANTA_FE_2022, CAR.SANTA_FE_HEV_2022, CAR.SANTA_FE_PHEV_2022):
